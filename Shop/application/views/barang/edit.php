0a12e53249897bf72b3fcae0186b86a4aff5fef9
triadesi30 <nuriapujihapsari@gmail.com>
2022-01-09 15:16:08 +0700

    memperbaiki view shop

--- Shop/application/views/barang/edit.php
+++ Shop/application/views/barang/edit.php
@@ -1,10 +1,11 @@
 <?php echo form_open('barang/edit');?>
-<?php echo form_hidden('id',$databarang[0]->id);?>
+<?php echo form_hidden('id_barang',$databarang[0]->id);?>
 
 <table>
     <tr><td>ID</td><td><?php echo form_input('',$databarang[0]->id,"disabled");?></td></tr>
-    <tr><td>NAMA</td><td><?php echo form_input('nama',$databarang[0]->nama);?></td></tr>
-    <tr><td>NOMOR</td><td><?php echo form_input('nomor',$databarang[0]->nomor);?></td></tr>
+    <tr><td>NAMA</td><td><?php echo form_input('nama_barang',$databarang[0]->nama);?></td></tr>
+    <tr><td>HARGA</td><td><?php echo form_input('harga_barang',$databarang[0]->harga);?></td></tr>
+    <tr><td>STOK</td><td><?php echo form_input('stok_barang',$databarang[0]->stok);?></td></tr>
     <tr><td colspan="2">
         <?php echo form_submit('submit','Simpan');?>
         <?php echo anchor('barang','Kembali');?></td></tr>
